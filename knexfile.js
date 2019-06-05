ff6c9c5179588cc7e6dcceb025866de5a9112970
Firealem Erko <firealemg@yahoo.com>
2019-06-05 17:13:21 -0400

    migrated the two tables

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: '.data/db.sqlite3'
+      filename: 'C:/Users/firea/Documents/LambdaProjects/webdb-iii-challenge/data/db.db3'
     },
     useNullAsDefault: true
   },
